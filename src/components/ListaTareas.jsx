1ba6b29bd10a759dae7fa2b42c70b4a96dd1a93c
yefrid18 <yefrid18@gmail.com>
2022-09-01 14:23:03 -0500

    componente form listo listar tareas

--- src/components/ListaTareas.jsx
+++ src/components/ListaTareas.jsx
@@ -1,6 +1,15 @@
+import { Tareas } from "./Tareas"
 
+Tareas
 export const ListaTareas = () => {
   return (
-    <div>ListaTareas</div>
+    <div className="md:w-1/2 lg:w-2/5 mx-5 mb-10 md:h-screen overflow-scroll">
+      <h2 className="font-black text-3xl text-center mb-10">
+        Lista de Tareas
+      </h2>
+    <Tareas />
+    <Tareas />
+    <Tareas />
+    </div>
   )
 }
---
+++ src/components/Tareas.jsx
@@ -0,0 +1,19 @@
+
+export const Tareas = () => {
+  return (
+    <div className="bg-white shadow-md px-5 py-10 rounded-lg mt-5">
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+            Titulo:{' '}
+           <span className="font-normal normal-case">Crear Api</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+            Fecha:{' '}
+           <span className="font-normal normal-case">01/09/2022</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+            Descripcion:{' '}
+           <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus voluptatum ipsum voluptatibus debitis adipisci quos facere maiores.</span>
+        </p>
+      </div>
+  )
+}
