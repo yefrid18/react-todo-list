10191f74a33b7c12ae9675d73e1d38f7f349e82b
yefrid18 <yefrid18@gmail.com>
2022-09-03 09:35:34 -0500

    organización y validacion tlista de taeas pendientes

--- src/components/ListaTareas.jsx
+++ src/components/ListaTareas.jsx
@@ -1,21 +1,34 @@
 import { Tareas } from "./Tareas"
 
 export const ListaTareas = ({tareas}) => {
+
   return (
     <div className="md:w-1/2 lg:w-2/5 mx-5 mb-10 md:h-screen overflow-scroll">
-      <h2 className="font-black text-3xl text-center mb-10">
-        Lista de Tareas
-      </h2>
 
-    {tareas.map( (tarea) => {
+      {tareas && tareas.length ? (
+      <>    
+
+      <h2 className="font-black text-3xl text-center mb-10">
+        Tareas pendientes
+      </h2>  
+
+      {tareas.map( (tarea) => {
         return(
             <Tareas  
                 key={tarea.id}               
                 tarea={tarea}            
             />  
         )
-    })}
+      })}
+      </>
+      
+      ) : (
 
+      <h2 className="font-black text-3xl text-center mb-10">
+        No tengo tareas pendientes
+      </h2>
+
+      )}
       
     </div>
   )
