a7ba22ac981b9d76e29d1b450416e9357786a97a
yefrid18 <yefrid18@gmail.com>
2022-09-03 09:50:43 -0500

    creacion de botones actualizar y eliminar basicos

--- src/components/Tareas.jsx
+++ src/components/Tareas.jsx
@@ -1,5 +1,6 @@
 export const Tareas = ({tarea}) => {
   return (
+
     <div className="bg-white shadow-md px-5 py-10 rounded-lg mt-5">
       <p className="font-bold mb-3 text-gray-700 uppercase">
         Titulo: <span className="font-normal normal-case">{tarea.titulo}</span>
@@ -13,6 +14,11 @@ export const Tareas = ({tarea}) => {
           {tarea.descripcion}
         </span>
       </p>
+      <div className="flex justify-between">
+        <button className="bg-green-600 hover:bg-green-700 mt-4 py-2 px-10 rounded-md text-white" type="button">Actualizar</button>
+        <button className="bg-red-600 hover:bg-red-700 mt-4 py-2 px-10 rounded-md text-white" type="button">Eliminar</button>
+      </div>
+
     </div>
   );
 };
